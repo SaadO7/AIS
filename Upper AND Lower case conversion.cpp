0fd3373ce1cdd996f92a19a91fc19638bbab2caf
Shaheer Asif <shaheerasif018@gmail.com>
2025-07-22 11:42:00 +0500

    Update Upper AND Lower case conversion.cpp

--- Upper AND Lower case conversion.cpp
+++ Upper AND Lower case conversion.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 int main() {
 	char a;
-	cout << "Enter a Character: ";
+	cout << "Enter a Character : ";
 	cin >> a;
 	int x = ((int)a);
-	if(x<=90) {
+	if(x <= 90) {
 		cout << "Character Remains Same: " << a;
 	}
-	else if(x>=97 && x<=122) {
+	else if(x >= 97 && x <= 122) {
 		x-=32;
 		char c= ((char)x);
-		cout << "Converted Charater: " << c;
+		cout << "Converted Charater : " << c;
 	}
 	return 0;
 }
